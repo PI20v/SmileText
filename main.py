70a2f79e859b7e49dbfd168dfa8f4777923fbcc7
tonylipch <91372267+tonylipch@users.noreply.github.com>
2022-04-05 14:24:30 +0300

    Algorithm for character substitution in text

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 import telebot
 
@@ -23,3 +24,33 @@ def telegram_bot(token):
 
 if __name__=='__main__':
     telegram_bot(token)
+=======
+import random
+
+
+def replacing_symbols(some_text):
+
+    emoji_list = ['😀', '😃', '😄', '😁',
+                  '👹','☠️','🤖','👨‍💻',
+                  '🤵','👓','👌🏻','🇺🇦',
+                  '🖥','🛠','⚙️','💵',
+                  '🩸','💿','💈','🪦',]
+
+    marks = '''!()-[]{};?@#$%:'"\,./^&;*_'''
+
+    for i in some_text:
+        if i in marks:
+             some_text = some_text.replace(i,random.choice(emoji_list))
+    return some_text
+
+
+if __name__ == '__main__':
+
+  print(  replacing_symbols('Приве! Я, Антон.'))
+
+
+
+
+
+
+>>>>>>> 71f5e7b (Algorithm for character substitution in text)
