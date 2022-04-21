6d173ce4378734c99f9fe801fc20cd3319e39710
tonylipch <91372267+tonylipch@users.noreply.github.com>
2022-04-21 15:57:49 +0300

    #5 Tony: Fixed bugs with merging 2 branches

--- main.py
+++ main.py
@@ -30,32 +30,3 @@ def telegram_bot(token):
 
 if __name__=='__main__':
     telegram_bot(token)
-=======
-import random
-
-
-def replacing_symbols(some_text):
-
-    emoji_list = ['😀', '😃', '😄', '😁',
-                  '👹','☠️','🤖','👨‍💻',
-                  '🤵','👓','👌🏻','🇺🇦',
-                  '🖥','🛠','⚙️','💵',
-                  '🩸','💿','💈','🪦',]
-
-    marks = '''!()-[]{};?@#$%:'"\,./^&;*_'''
-
-    for i in some_text:
-        if i in marks:
-             some_text = some_text.replace(i,random.choice(emoji_list))
-    return some_text
-
-
-if __name__ == '__main__':
-
-  print(  replacing_symbols('Приве! Я, Антон.'))
-
-
-
-
-
-
