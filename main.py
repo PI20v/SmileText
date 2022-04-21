597ae1fb9a62f399993d1f0f5eda1f93977e82e7
Nikita Obukhov <92372780+zx300xz@users.noreply.github.com>
2022-04-21 15:40:29 +0300

    #2 Testin telegram bot

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import telebot
+
+import random
+
+
+
+def telegram_bot(token):
+
+    bot = telebot.TeleBot(token)
+
+    @bot.message_handler(commands=["start"])
+    def start_message (message):
+
+        bot.send_message(message.chat.id, 'test')
+
+
+
+    bot.polling()
+
+
+
+if __name__=='__main__':
+    telegram_bot(token)
